4e0337f3c68885e0b2f20067fad266bdd54de540
Clinton-stack <56035975+Clinton-stack@users.noreply.github.com>
2024-04-18 04:34:06 +0200

    hero section almost complete

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ import { IoIosArrowDropright } from "react-icons/io";
 
 export default function Home() {
   return (
-    <Box bgColor='#2e294e' pl={40} pr={40}>
-      <HStack spacing="10px" h='50px'>
+    <Container maxW='1200px'>
+      <HStack spacing="10px" h='50px' align='center' mt='30px'>
         <Link
           href="https://www.linkedin.com/in/clinton-emenari-8aba67108/"
           target="_blank"
@@ -28,8 +28,8 @@ export default function Home() {
 
         <Box>
           <Image
-            w={{ base: "250px", md: "300px", lg: "320px"}}
-            maxW="100%"
+            w={{ base: "250px", md: "300px", lg: "320px" }}
+            minW="200px"
             h="auto"
             borderRadius="full"
             src="/clinton2.png"
@@ -37,23 +37,26 @@ export default function Home() {
             bgColor='#ffd400'
           />
         </Box>
-        <VStack w='700px' justify={{base: 'center', md: 'center', lg: 'flex-start', xl: 'flex-start'}}
->
-          <Text className="heroText" fontSize={{base: '50px', md: '60px', lg: '65px'}}>
-            Clinton Emenari
-          </Text>
-          <Text fontSize="xl" color='#ffd400'>
-            SOFTWARE DEVELOPER
-          </Text>
-          <Text fontSize="l"  mt="10px">
-            &#34;I am an enthusiastic software developer driven by a passion for
-            problem-solving through innovative software solutions.&#34;
-          </Text>
-          <Button size='md' borderRadius='full' bgGradient="linear(to-tr, #ffd400, #fcf3cc)" maxW='210px' p='30px'>Download Résumé</Button>
-        </VStack>
+        <HStack w='600px'>
+          <VStack  align={{ md: 'left', lg: 'left' }} spacing={5}>
+            <Heading className="heroText" fontSize={{ base: '50px', md: '60px', lg: '65px' }} >
+              Clinton Emenari
+            </Heading>
+            <Text className="devText">
+              SOFTWARE DEVELOPER
+            </Text>
+            <Text className="regularText">
+              &#34;I am an enthusiastic software developer driven by a passion for
+              problem-solving through innovative software solutions.&#34;
+            </Text>
+            <Button size='md' borderRadius='full' bgGradient="linear(to-tr, #ffd400, #fcf3cc)" maxW='210px' p='30px'>Download Résumé</Button>
+          </VStack>
+          <IoIosArrowDropright className="icons" />
+        </HStack>
+
 
       </div>
 
-    </Box>
+    </Container>
   );
 }
