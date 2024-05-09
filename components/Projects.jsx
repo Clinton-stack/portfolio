b22fc8618003d373e6584347f69f0b63fc1eaa6f
Clinton-stack <56035975+Clinton-stack@users.noreply.github.com>
2024-05-09 19:47:00 +0200

    edited projects'

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,181 +4,50 @@ import { Box, Card, CardBody, Heading, Image, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
-import { AnimatePresence, motion } from "framer-motion";
 import { Slide } from "react-awesome-reveal";
+import { projectsData } from "./constants/projectsdata";
 
 export default function Projects() {
   return (
     <div className="projects">
-      <Slide direction="left" triggerOnce fraction={0.7}>
+     {projectsData.map((project) => (
+      <Slide direction={`${ project.id % 2 === 0 ? 'right': 'left'}`} triggerOnce fraction={0.7} key={project.id}>   
         <Card maxW="lg" bgColor="none" className="cards">
           <CardBody>
             <Box
               as={Link}
-              href="https://ingenious-technology.org/"
+              href={project.link}
               target="_blank"
               rel="noopener noreferrer"
               passHref
               pos="relative"
             >
               <Image
-                src="/ingeniouswebsite.png"
-                alt="Ingenious Website"
-                w="100%"
-                maxH="300px"
-              />
-              <FaExternalLinkAlt className="link-icon" />
-            </Box>
-
-            <VStack mt={3} align={{ base: "center", md: "flex-start" }}>
-              <Heading as="h3" size="l" className="headerText">
-                PROJECT INFO
-              </Heading>
-              <p className="regularText">
-                An informative website for Ingenious Technologies. Built with
-                React, Next.js, Chakra UI and AWS Technologies
-              </p>
-              <Heading as="h3" size="l" className="headerText">
-                CLIENT
-              </Heading>
-              <p className="regularText">Ingenious Technologies</p>
-              <Heading as="h3" size="l" className="headerText">
-                TECHNOLOGIES USED
-              </Heading>
-              <p className="regularText">
-                React, Next.js, Chakra UI, Tailwind, AWS Lamba, AWS S3, AWS
-                Cloudwatch
-              </p>
-            </VStack>
-          </CardBody>
-        </Card>
-      </Slide>
-      <Slide direction="right" triggerOnce fraction={0.7}>
-        <Card maxW="lg" bgColor="none" className="cards">
-          <CardBody>
-            <Box
-              as={Link}
-              href="https://food-lovers-app.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              passHref
-              pos="relative"
-            >
-              <Image
-                src="/foodlovers.png"
-                alt="Food Lovers Website"
+                src={project.image}
+                alt={project.name}
                 w="100%"
                 maxH="260px"
               />
               <FaExternalLinkAlt className="link-icon" />
             </Box>
-
             <VStack mt={3} align={{ base: "center", md: "flex-start" }}>
               <Heading as="h3" size="l" className="headerText">
                 PROJECT INFO
               </Heading>
-              <p className="regularText">
-                A website for food lovers to connect and share their favorite
-                food recipes. It was built with React, Next.js and Tailwind CSS
-              </p>
+              <p className="regularText">{project.description}</p>
               <Heading as="h3" size="l" className="headerText">
                 CLIENT
               </Heading>
-              <p className="regularText">Food Lovers</p>
+              <p className="regularText">{project.client}</p>
               <Heading as="h3" size="l" className="headerText">
                 TECHNOLOGIES USED
               </Heading>
-              <p className="regularText">React, Next.js, Tailwind, AWS S3,</p>
+              <p className="regularText">{project.technologies}</p>
             </VStack>
           </CardBody>
         </Card>
-      </Slide>
-
-      <Slide direction="left" triggerOnce fraction={0.7}>
-        <Card maxW="lg" bgColor="none" className="cards">
-          <CardBody>
-            <Box
-              as={Link}
-              href="https://digitalsocietyschool.org/insight/the-smart-talking-plant-pot/"
-              target="_blank"
-              rel="noopener noreferrer"
-              passHref
-              pos="relative"
-            >
-              <Image
-                src="/plantpot.jpeg"
-                alt="Smart Plant pot"
-                w="100%"
-                maxH="260px"
-              />
-              <FaExternalLinkAlt className="link-icon" />
-            </Box>
-
-            <VStack mt={3} align={{ base: "center", md: "flex-start" }}>
-              <Heading as="h3" size="l" className="headerText">
-                PROJECT INFO
-              </Heading>
-              <p className="regularText">
-                A smart plant pot built to help educate and empower Dutch
-                citizen on how to combat night time heat in the Netherlands. The
-                prototype was built using C and Arduino
-              </p>
-              <Heading as="h3" size="l" className="headerText">
-                CLIENT
-              </Heading>
-              <p className="regularText">Digital School Society</p>
-              <Heading as="h3" size="l" className="headerText">
-                TECHNOLOGIES USED
-              </Heading>
-              <p className="regularText">C, Arduino</p>
-            </VStack>
-          </CardBody>
-        </Card>
-      </Slide>
-
-      <Slide direction="right" triggerOnce fraction={0.7}>
-        <Card maxW="lg" bgColor="none" className="cards">
-          <CardBody>
-            <Box
-              as={Link}
-              href="https://ingenious-ams.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              passHref
-              pos="relative"
-            >
-              <Image
-                src="/ingenious-ams.png"
-                alt="Asset Managment System"
-                w="100%"
-                maxH="260px"
-              />
-              <FaExternalLinkAlt className="link-icon" />
-            </Box>
-
-            <VStack mt={3} align={{ base: "center", md: "flex-start" }}>
-              <Heading as="h3" size="l" className="headerText">
-                PROJECT INFO
-              </Heading>
-              <p className="regularText">
-                An asset management system for managing various kind of assest
-                for Ingenious Technologies. Front-end Built using React, Next.js
-                and Chakra UI. Project still in progress
-              </p>
-              <Heading as="h3" size="l" className="headerText">
-                CLIENT
-              </Heading>
-              <p className="regularText">Ingenious Technologies</p>
-              <Heading as="h3" size="l" className="headerText">
-                TECHNOLOGIES USED
-              </Heading>
-              <p className="regularText">React, Next.js, Chakra UI,</p>
-            </VStack>
-          </CardBody>
-        </Card>
-      </Slide>
-
-      
+        </Slide>
+     ))} 
     </div>
   );
 }
---
+++ components/constants/projectsdata.js
@@ -0,0 +1,38 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: "Ingenious Website",
+        description: "An informative website for Ingenious Technologies. Built with React, Next.js, Chakra UI and AWS Technologies",
+        client: "Ingenious Technologies",
+        technologies: "React, Next.js, Chakra UI, Tailwind, AWS Lamba, AWS S3, AWS Cloudwatch",
+        image: "/ingeniouswebsite.png",
+        link: "https://ingenious-technology.org/",
+    },
+    {
+        id: 2,
+        name: "Food Lovers App",
+        description: "A food recipe app built with React, Next.js, Chakra UI and TheMealDB API",
+        client: "Personal Project",
+        technologies: "React, Next.js, Chakra UI, Tailwind, TheMealDB API",
+        image: "/foodlovers.png",
+        link: "https://food-lovers-app.vercel.app/",
+    },
+    {
+        id: 3,
+        name: "Smart Plant Pot",
+        description: " A smart plant pot built to help educate and empower Dutch citizen on how to combat night time heat in the Netherlands. The prototype was built using C and Arduino",
+        client: "Digital Society School",
+        technologies: "C, Arduino",
+        image: "/plantpot.jpeg",
+        link: "https://digitalsocietyschool.org/insight/the-smart-talking-plant-pot/",
+    },
+    {
+        id: 4,
+        name: "Ingenious Asset Management System",
+        description: "An asset management system for managing various kind of assest for Ingenious Technologies. Front-end Built using React, Next.js and Chakra UI. Project still in progress",
+        client: "Ingenious Technologies",
+        technologies: "React, Next.js, Chakra UI, Tailwind",
+        image: "/ingenious-ams.png",
+        link: "https://ingenious-ams.vercel.app/",
+    }
+]
